2b317cb2ac62089cfbcf7c49bbd30ff87b4aaa1b
Ross Cohen <rosscohen1990@gmail.com>
2023-05-26 11:55:21 -0400

    Update index.ts with english text

--- server/index.ts
+++ server/index.ts
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(cors())
 
-// 获取静态目录
+// "Retrieve static directory"
 app.use(express.static(path.join(__dirname, '../dist')))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -15,12 +15,12 @@ app.all('/api/*', (req, res) => {
   res.status(404).json({ code: -1, data: [], message: 'The current access API address does not exist' })
 })
 
-// 渲染页面
+// "Render page"
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist', 'index.html'))
 })
 
-// 启动服务器
+// "Start server"
 app.listen(3333, () => {
   console.log('Server is running on port 3333')
 })
