ee252e451333ddb5ec87d61fab3059973139c34e
Ross Cohen <rosscohen1990@gmail.com>
2023-05-26 13:44:59 -0400

    Update getEmailPre.ts english

--- src/utils/getEmailPre.ts
+++ src/utils/getEmailPre.ts
@@ -1,11 +1,11 @@
 export function getEmailPre(str?: string) {
   if (!str) return ''
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/ // 邮箱正则表达式
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/ // Regular expression of mailboxes
   if (emailRegex.test(str)) {
-    // 如果是邮箱，则返回@前的字符串
+    // If it is an mailbox, return@如果 string
     return str.split('@')[0]
   } else {
-    // 如果不是邮箱，则返回整个字符串
+    // If it is not an mailbox, return the entire string
     return str
   }
 }
